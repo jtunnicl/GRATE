086a62031fd2ab0645da880fcf2725497949291f
Chris Scott <chris.scott@nesi.org.nz>
2018-12-10 12:02:02 +1300

    Option to generate profiling graph during test

--- tests/run_test.cmake
+++ tests/run_test.cmake
@@ -6,6 +6,10 @@ message(STATUS "  Test run directory: ${TEST_RUN_DIR}")
 message(STATUS "  Test src directory: ${TEST_SRC_DIR}")
 message(STATUS "  Test binary: ${TEST_BINARY}")
 message(STATUS "  Compare binary: ${COMPARE_BINARY}")
+message(STATUS "  Profiling: ${DOING_PROFILING}")
+message(STATUS "  Gprof program: ${GPROF_PROGRAM}")
+message(STATUS "  Gprof2dot program: ${GPROF2DOT_PROGRAM}")
+message(STATUS "  Dot program: ${DOT_PROGRAM}")
 
 #
 # number of steps to run the test for
@@ -38,13 +42,33 @@ if (status)
 endif (status)
 
 #
+# profiling output
+#
+if (DOING_PROFILING)
+    message(STATUS "Generating profiling output")
+
+    # run gprof
+    execute_process(
+        COMMAND ${CMAKE_COMMAND} -E chdir ${TEST_RUN_DIR} ${GPROF_PROGRAM} ${TEST_BINARY}
+        OUTPUT_FILE ${TEST_RUN_DIR}/gprof.out
+    )
+
+    # run gprof2dot
+    execute_process(
+        COMMAND ${CMAKE_COMMAND} -E chdir ${TEST_RUN_DIR}
+                ${GPROF2DOT_PROGRAM} --colour-nodes-by-selftime --strip gprof.out
+        OUTPUT_FILE ${TEST_RUN_DIR}/gprof2dot.out
+    )
+
+    # run dot
+    execute_process(
+        COMMAND ${CMAKE_COMMAND} -E chdir ${TEST_RUN_DIR} ${DOT_PROGRAM} -Tpng -o profile.png gprof2dot.out
+    )
+endif()
+
+#
 # check the results
 #
-#execute_process(
-#    COMMAND ${CMAKE_COMMAND} -E compare_files ${TEST_RUN_DIR}/Run_Results.txt
-#                                              ${TEST_SRC_DIR}/Run_Results_Ref800.txt
-#    RESULT_VARIABLE status
-#)
 execute_process(
     COMMAND ${COMPARE_BINARY} ${TEST_SRC_DIR}/Run_Results_Ref800.txt
                               ${TEST_RUN_DIR}/Run_Results.txt
